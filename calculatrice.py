dcae269faad232676b14eaaacbe29791e91370af
Alexandre Bourre <alexandre.bourre@ymail.com>
2021-04-27 15:24:38 +0200

    last modif

--- calculatrice.py
+++ calculatrice.py
@@ -2,6 +2,7 @@ def ask_user(sentence = "Saisir un chiffre"): # do_something, refactor name def
     choice = input(f"""{sentence}\n>""") #transform string on int
     return choice
 
+
 def addition(number): # do_something, refactor name def
     list_numbers = []
     while number.isdigit():
@@ -16,7 +17,7 @@ def multiplication(number):
     while number.isdigit():
         if number.isdigit():
             list_numbers.append((int(number))) # do_something ajouter la methode int
-        number = ask_user("Saisir un ciffre à multiplier ou clicker sur '=' ")
+        number = ask_user("Saisir un chiffre à multiplier ou clicker sur '=' ")
     for list_number, index in zip(list_numbers,list(range(len(list_numbers)))): # refactoriser
         if index == 0: # do_something, transformer en int
             result = list_number
